dd21730564f4d915a8f5a63f68abae16b97da492
Autumn <autumncbullard@gmail.com>
2017-11-20 01:59:43 -0500

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js|.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 25000,
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          use:['css-loader', 'sass-loader'],
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new ExtractTextPlugin({ filename: 'style.css', allChunks: true })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
