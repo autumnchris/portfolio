d78496c109be554a9270b4f315ebf6a9de0a76dd
Autumn <autumncbullard@gmail.com>
2017-11-22 03:47:32 -0500

    Create a fixed navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 export default class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid"></div>
+      <div>
+        <Navbar fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/" target="_self"><h1>Autumn Bullard</h1></a>
+            </Navbar.Brand>
+            <Navbar.Toggle>
+              <span className="fa fa-bars fa-2x"></span>
+            </Navbar.Toggle>
+          </Navbar.Header>
+          <Navbar.Collapse>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="#about" target="_self">About</NavItem>
+                <NavItem eventKey={2} href="#portfolio" target="_self">Portfolio</NavItem>
+                <NavItem eventKey={3} href="#contact" target="_self">Contact</NavItem>
+              </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
