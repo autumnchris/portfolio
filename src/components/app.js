84a7799d0d2b320c3dbaa1c7033e05c34f298916
Autumn <autumncbullard@gmail.com>
2017-11-25 21:22:03 -0500

    Add markup and styling for a page footer

--- src/components/app.js
+++ src/components/app.js
@@ -69,6 +69,7 @@ export default class App extends Component {
             </section>
           </ScrollableAnchor>
         </main>
+        <footer>Coded by <a href="../resume-portfolio" target="_self">Autumn Bullard</a></footer>
       </div>
     );
   }
