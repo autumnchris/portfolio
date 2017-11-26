78f211394c49569143e1373d8c0c2c141fccf80a
Autumn <autumncbullard@gmail.com>
2017-11-25 21:48:08 -0500

    Add markup and styling for the About section

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
+const avatar = require('.././images/avatar.jpg');
+
 export default class App extends Component {
 
   constructor(props) {
@@ -45,6 +47,29 @@ export default class App extends Component {
           <ScrollableAnchor id={'about'}>
             <section className="center-block">
               <h2>About the Developer</h2>
+              <div className="row">
+                <div className="col-sm-4">
+                  <img src={avatar} className="img-responsive img-circle center-block" alt="avatar image" id="avatar" />
+                  <ul className="list-inline text-center">
+                    <li>
+                      <a href="https://www.linkedin.com/in/autumncbullard" target="_blank" id="linkedin-icon">
+                        <span className="fa fa-linkedin-square fa-3x"></span>
+                        <span className="sr-only">LinkedIn profile</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://github.com/autumnchris" target="_blank" id="github-icon">
+                        <span className="fa fa-github fa-3x"></span>
+                        <span className="sr-only">GitHub profile</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-sm-8">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean est sem, dapibus nec volutpat sed, euismod in risus. Nullam nec nibh arcu. Phasellus dignissim sed lacus ut semper. Nulla efficitur ex vitae dignissim interdum. Aliquam cursus dapibus tortor, quis laoreet erat convallis vitae. Aliquam erat volutpat. Morbi sit amet magna finibus, rhoncus turpis sed, volutpat tellus.</p>
+                  <p>Duis non dui pellentesque, dignissim metus vitae, venenatis ex. Pellentesque ac ligula augue. Morbi cursus nibh libero, vel ullamcorper nisi ultricies molestie. Ut semper augue nibh, dignissim fermentum eros pretium sit amet. Duis iaculis tristique laoreet. Quisque malesuada eros a elit convallis, imperdiet feugiat felis pulvinar. Maecenas non vehicula ante. Nulla mattis turpis quis diam luctus facilisis. Aliquam vitae condimentum augue, non consectetur metus. Vestibulum non lobortis mi. Proin nec dui nec augue viverra vehicula. Phasellus feugiat fringilla scelerisque.</p>
+                </div>
+              </div>
             </section>
           </ScrollableAnchor>
           <ScrollableAnchor id={'portfolio'}>
