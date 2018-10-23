3f1886f29379faac39a22f4526062c455eda7ac9
Autumn <autumncbullard@gmail.com>
2018-10-23 17:34:02 -0400

    Set up Mongoose

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const webpack = require('webpack');
 const config = require('./webpack.config.js');
 
 const app = express();
 const compiler = webpack(config);
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
