2bfb7255596be141b91ed5d5d18a8fa62288b796
autumnchris <autumncbullard@gmail.com>
2020-04-07 18:01:24 -0400

    Update smooth scroll for internal links

--- README.md
+++ README.md
@@ -5,16 +5,17 @@
 ## Built With
 * [React.js](https://reactjs.org)
 * [Sass](http://sass-lang.com)
-* [Webpack](https://webpack.js.org)
 * [Express.js](https://expressjs.com)
 * [Mongoose](https://mongoosejs.com)
 * [MongoDB](https://www.mongodb.com)
+* [Webpack](https://webpack.js.org)
 * [EJS](https://ejs.co)
 * [axios](https://github.com/axios/axios)
+* AJAX
 * [Babel](https://babeljs.io)
 * [Font Awesome](https://fontawesome.com)
 * [Google Material Icons](https://material.io/icons)
-* [react-scrollable-anchor](https://github.com/gabergg/react-scrollable-anchor)
+* [react-scroll](https://github.com/fisshy/react-scroll)
 * [Google Fonts](https://fonts.google.com)
 * [Esphimere font](https://www.dafont.com/esphimere.font)
 
--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Projects from './projects';
 import axios from 'axios';
-import ScrollableAnchor from 'react-scrollable-anchor';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 const avatar = require('../images/avatar.jpg');
 
@@ -48,72 +48,66 @@ export default class App extends Component {
       <div className="body">
         <nav>
           <h1>
-            <a href="/" target="_self">Autumn Bullard</a>
+            <a onClick={scroll.scrollToTop}>Autumn Bullard</a>
           </h1>
           <div className="nav-menu">
             <button type="button" onClick={() => this.toggleNavIcon()}>
               <span className="material-icons">{this.state.toggleClass}</span>
             </button>
             <div className={`nav-links ${this.state.navLinksClass}`}>
-              <a href="#about" target="_self">About</a>
-              <a href="#portfolio" target="_self">Portfolio</a>
-              <a href="#contact" target="_self">Contact</a>
+              <Link to="about" smooth duration={1000}>About</Link>
+              <Link to="portfolio" smooth duration={1000}>Portfolio</Link>
+              <Link to="contact" smooth duration={1000}>Contact</Link>
             </div>
           </div>
         </nav>
         <main>
-          <ScrollableAnchor id="about">
-            <section className="about">
-              <h2>About the Developer</h2>
-              <div className="content">
-                <div className="col">
-                  <img src={avatar} className="avatar" alt="photo of Autumn Bullard" />
-                  <div className="social-icons">
-                    <a href="https://www.linkedin.com/in/autumncbullard" target="_blank" className="linkedin-icon" title="LinkedIn" aria-label="LinkedIn profile">
-                      <span className="fab fa-linkedin fa-3x"></span>
-                    </a>
-                    <a href="https://github.com/autumnchris" target="_blank" className="github-icon" title="GitHub" aria-label="GitHub profile">
-                      <span className="fab fa-github fa-3x"></span>
-                    </a>
-                  </div>
-                </div>
-                <div className="col">
-                  <p>I am a front-end web developer residing in Jacksonville Beach, Florida. I prioritize accessibility and responsiveness in my markup and design. It's my aim to keep up to date with the latest technologies while also using techniques that I know work well. Producing clean and organized code is also of high priority to me. It's not enough for me just to make something work. It's important to understand what I have made. Does it work effectively? Can I easily apply this process to a future project? Have I considered every scenario?</p>
-                  <h3>Certifications:</h3>
-                  <ul>
-                    <li><a href="https://www.freecodecamp.org/certification/autumnchris/front-end-libraries" target="_blank">Front End Libraries</a> from freeCodeCamp</li>
-                    <li><a href="https://www.freecodecamp.org/certification/autumnchris/javascript-algorithms-and-data-structures" target="_blank">JavaScript Algorithms and Data Structures</a> from freeCodeCamp</li>
-                    <li><a href="https://www.freecodecamp.org/certification/autumnchris/responsive-web-design" target="_blank">Responsive Web Design</a> from freeCodeCamp</li>
-                    <li><a href="https://www.freecodecamp.org/certification/autumnchris/data-visualization" target="_blank">Data Visualization</a> from freeCodeCamp</li>
-                    <li><a href="https://www.freecodecamp.org/certification/autumnchris/apis-and-microservices" target="_blank">APIs and Microservices</a> from freeCodeCamp</li>
-                  </ul>
+          <section className="about" id="about">
+            <h2>About the Developer</h2>
+            <div className="content">
+              <div className="col">
+                <img src={avatar} className="avatar" alt="photo of Autumn Bullard" />
+                <div className="social-icons">
+                  <a href="https://www.linkedin.com/in/autumncbullard" target="_blank" className="linkedin-icon" title="LinkedIn" aria-label="LinkedIn profile">
+                    <span className="fab fa-linkedin fa-3x fa-fw"></span>
+                  </a>
+                  <a href="https://github.com/autumnchris" target="_blank" className="github-icon" title="GitHub" aria-label="GitHub profile">
+                    <span className="fab fa-github fa-3x fa-fw"></span>
+                  </a>
                 </div>
               </div>
-            </section>
-          </ScrollableAnchor>
-          <ScrollableAnchor id="portfolio">
-            <section className="portfolio">
-              <h2>Some of My Work</h2>
-              <Projects projects={this.state.portfolio} />
-              <p className="message error-message" style={this.state.errorStyle}><span className="fa fa-exclamation-circle fa-lg fa-fw"></span> Unable to load portfolio projects.</p>
-            </section>
-          </ScrollableAnchor>
-          <ScrollableAnchor id="contact">
-            <section className="contact">
-              <h2>Contact Me</h2>
-              <div className="content">
-                <div className="col">
-                  <h3>Email Me At...</h3>
-                  <div className="email"><span className="far fa-envelope fa-fw"></span> <a href="mailto:autumncbullard@gmail.com" target="_self">autumncbullard@gmail.com</a></div>
-                </div>
-                <div className="col">
-                  <h3>Find Me On...</h3>
-                  <a className="social-links linkedin-profile" href="https://www.linkedin.com/in/autumncbullard" target="_blank"><span className="fab fa-linkedin fa-fw"></span> LinkedIn</a>
-                  <a className="social-links github-profile" href="https://github.com/autumnchris" target="_blank"><span className="fab fa-github fa-fw"></span> GitHub</a>
-                </div>
+              <div className="col">
+                <p>I am a front-end web developer residing in Jacksonville Beach, Florida. I prioritize accessibility and responsiveness in my markup and design. It's my aim to keep up to date with the latest technologies while also using techniques that I know work well. Producing clean and organized code is also of high priority to me. It's not enough for me just to make something work. It's important to understand what I have made. Does it work effectively? Can I easily apply this process to a future project? Have I considered every scenario?</p>
+                <h3>Certifications:</h3>
+                <ul>
+                  <li><a href="https://www.freecodecamp.org/certification/autumnchris/front-end-libraries" target="_blank">Front End Libraries</a> from freeCodeCamp</li>
+                  <li><a href="https://www.freecodecamp.org/certification/autumnchris/javascript-algorithms-and-data-structures" target="_blank">JavaScript Algorithms and Data Structures</a> from freeCodeCamp</li>
+                  <li><a href="https://www.freecodecamp.org/certification/autumnchris/responsive-web-design" target="_blank">Responsive Web Design</a> from freeCodeCamp</li>
+                  <li><a href="https://www.freecodecamp.org/certification/autumnchris/data-visualization" target="_blank">Data Visualization</a> from freeCodeCamp</li>
+                  <li><a href="https://www.freecodecamp.org/certification/autumnchris/apis-and-microservices" target="_blank">APIs and Microservices</a> from freeCodeCamp</li>
+                </ul>
               </div>
-            </section>
-          </ScrollableAnchor>
+            </div>
+          </section>
+          <section className="portfolio" id="portfolio">
+            <h2>Some of My Work</h2>
+            <Projects projects={this.state.portfolio} />
+            <p className="message error-message" style={this.state.errorStyle}><span className="fa fa-exclamation-circle fa-lg fa-fw"></span> Unable to load portfolio projects.</p>
+          </section>
+          <section className="contact" id="contact">
+            <h2>Contact Me</h2>
+            <div className="content">
+              <div className="col">
+                <h3>Email Me At...</h3>
+                <div className="email"><span className="far fa-envelope fa-fw"></span> <a href="mailto:autumncbullard@gmail.com" target="_self">autumncbullard@gmail.com</a></div>
+              </div>
+              <div className="col">
+                <h3>Find Me On...</h3>
+                <a className="social-links linkedin-profile" href="https://www.linkedin.com/in/autumncbullard" target="_blank"><span className="fab fa-linkedin fa-fw"></span> LinkedIn</a>
+                <a className="social-links github-profile" href="https://github.com/autumnchris" target="_blank"><span className="fab fa-github fa-fw"></span> GitHub</a>
+              </div>
+            </div>
+          </section>
         </main>
       </div>
     );
