589883043ff20b172b5b57d5fbd6b3005c21d24f
autumnchris <autumncbullard@gmail.com>
2020-04-16 22:12:26 -0400

    Update API presentation

--- controllers/project-controller.js
+++ controllers/project-controller.js
@@ -2,7 +2,7 @@ const Project = require('../models/project');
 const Framework = require('../models/framework');
 
 exports.fetchProjects = (req, res, next) => {
-  Project.find({}, 'title description icon frameworks date').populate({
+  Project.find({}, 'title description icon frameworks date -_id').populate({
     path: 'frameworks',
     select: 'name demo sourceCode -_id',
     options: {
