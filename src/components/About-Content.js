6cc1abd5548df5dc679d864bc7dcc2f411f28333
autumnchris <autumncbullard@gmail.com>
2022-12-07 21:43:05 -0500

    Sort skills in custom order

--- src/components/About-Content.js
+++ src/components/About-Content.js
@@ -16,15 +16,15 @@ const AboutContent = ({ aboutContent }) => {
           <h3>Skills</h3>
           <div className="skill-group">
             <h4>Technologies</h4>
-            <ul>{aboutContent.skills.filter(skill => skill.skillType === 'Technologies').map(techSkill => <Skill key={techSkill._id} skill={techSkill} />)}</ul>
+            <ul>{aboutContent.skills.filter(skill => skill.skillType === 'Technologies').sort((a, b) => a.listOrder - b.listOrder).map(techSkill => <Skill key={techSkill._id} skill={techSkill} />)}</ul>
           </div>
           <div className="skill-group">
             <h4>Development</h4>
-            <ul>{aboutContent.skills.filter(skill => skill.skillType === 'Development').map(devSkill => <Skill key={devSkill._id} skill={devSkill} />)}</ul>
+            <ul>{aboutContent.skills.filter(skill => skill.skillType === 'Development').sort((a, b) => a.listOrder - b.listOrder).map(devSkill => <Skill key={devSkill._id} skill={devSkill} />)}</ul>
           </div>
           <div className="skill-group">
             <h4>Soft Skills</h4>
-            <ul>{aboutContent.skills.filter(skill => skill.skillType === 'Soft Skills').map(softSkill => <Skill key={softSkill._id} skill={softSkill} />)}</ul>
+            <ul>{aboutContent.skills.filter(skill => skill.skillType === 'Soft Skills').sort((a, b) => a.listOrder - b.listOrder).map(softSkill => <Skill key={softSkill._id} skill={softSkill} />)}</ul>
           </div>
         </div>
       </React.Fragment>
