3e04cb90f03a090992bf4ef7e38abd2202595332
autumnchris <autumncbullard@gmail.com>
2025-01-29 13:01:27 -0500

    Remove link from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer>Created by <a href="https://autumnchris.github.io/portfolio" target="_self">Autumn Bullard</a> &copy; {new Date().getFullYear()}</footer>;
+  return <footer>Created by Autumn Bullard &copy; {new Date().getFullYear()}</footer>;
 }
 
 export default Footer;
