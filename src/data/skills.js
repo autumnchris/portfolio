8dfc072ebc696b517fd0c042a27faea7d0bdf7df
autumnchris <autumncbullard@gmail.com>
2025-10-06 17:04:33 -0400

    Update Skills section

--- src/data/skills.js
+++ src/data/skills.js
@@ -61,102 +61,162 @@ const skills = [
   },
   {
     id: 13,
-    name: 'Adobe Photoshop',
+    name: 'Jest',
     type: 'Technologies'
   },
   {
     id: 14,
+    name: 'SQL',
+    type: 'Technologies'
+  },
+    {
+    id: 15,
+    name: 'Postgres',
+    type: 'Technologies'
+  },
+    {
+    id: 16,
+    name: 'WordPress',
+    type: 'Technologies'
+  },
+    {
+    id: 17,
+    name: 'PHP',
+    type: 'Technologies'
+  },
+  {
+    id: 18,
+    name: 'Adobe Photoshop',
+    type: 'Technologies'
+  },
+  {
+    id: 19,
     name: 'Front-End Web Development',
     type: 'Development'
   },
   {
-    id: 15,
+    id: 20,
     name: 'Responsive Web Design',
     type: 'Development'
   },
   {
-    id: 16,
+    id: 21,
     name: 'Web Accessibility',
     type: 'Development'
   },
   {
-    id: 17,
+    id: 22,
     name: 'UI/UX',
     type: 'Development'
   },
   {
-    id: 18,
+    id: 23,
     name: 'AJAX',
     type: 'Development'
   },
   {
-    id: 19,
+    id: 24,
     name: 'REST APIs',
     type: 'Development'
   },
   {
-    id: 20,
+    id: 25,
     name: 'Debugging',
     type: 'Development'
   },
   {
-    id: 21,
-    name: 'Full Stack Web Development',
+    id: 26,
+    name: 'Full-Stack Web Development',
     type: 'Development'
   },
   {
-    id: 22,
+    id: 27,
     name: 'Wireframing',
     type: 'Development'
   },
   {
-    id: 23,
+    id: 28,
     name: 'MERN Stack',
     type: 'Development'
   },
   {
-    id: 24,
+    id: 29,
     name: 'Unit Testing',
     type: 'Development'
   },
   {
-    id: 25,
+    id: 30,
+    name: 'WordPress Theme Development',
+    type: 'Development'
+  },
+  {
+    id: 31,
     name: 'Organizational Skills',
     type: 'Other Skills'
   },
   {
-    id: 26,
+    id: 32,
     name: 'Critical Thinking',
     type: 'Other Skills'
   },
   {
-    id: 27,
+    id: 33,
     name: 'Problem Solving',
     type: 'Other Skills'
   },
   {
-    id: 28,
+    id: 34,
     name: 'Writing',
     type: 'Other Skills'
   },
   {
-    id: 29,
+    id: 35,
+    name: 'Editing',
+    type: 'Other Skills'
+  },
+  {
+    id: 36,
+    name: 'Technical Writing',
+    type: 'Other Skills'
+  },
+  {
+    id: 37,
     name: 'Design',
     type: 'Other Skills'
   },
   {
-    id: 30,
+    id: 38,
     name: 'Drawing',
     type: 'Other Skills'
   },
   {
-    id: 31,
+    id: 39,
+    name: 'Data Entry',
+    type: 'Other Skills'
+  },
+  {
+    id: 40,
+    name: 'Content Quality',
+    type: 'Other Skills'
+  },
+  {
+    id: 41,
     name: 'Self-Management',
     type: 'Other Skills'
   },
   {
-    id: 32,
+    id: 42,
     name: 'Creativity',
+    type: 'Other Skills'
+  },
+  {
+    id: 43,
+    name: 'Collaboration',
+    type: 'Other Skills'
+  },
+  {
+    id: 44,
+    name: 'Communication',
     type: 'Other Skills'
   }
 ];
